86afd80fc81674d756a38e22b046c0e56daa6211
iduk <iduk.design@gmail.com>
2025-01-11 18:17:26 +0900

    update settings.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import ThreeText from '@/components/ThreeText';
-
 export default function IndexPage() {
     return (
         <>
             <h1>index 첫페이지</h1>
 
-            <ThreeText />
+
         </>
     )
 }
