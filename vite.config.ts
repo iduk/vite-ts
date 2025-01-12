42bb67f965c48287c30eb0bd5454d02b28fa0ab9
iduk <iduk.design@gmail.com>
2025-01-12 16:33:06 +0900

    ollama test

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:11434',
+                target: 'http://localhost:3000',
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, ''),
             },
