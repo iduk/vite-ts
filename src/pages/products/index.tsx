77fb2960f5fd09c2188f62050819be75d4725099
iduk <iduk.design@gmail.com>
2025-01-13 11:47:20 +0900

    product link

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,21 @@
 export default function ProductIndex() {
-    return <div>src/pages/products 페이지</div>
+    return (
+        <>
+            <h1>Product Page</h1>
+            <div>
+                <h2>Product List</h2>
+                <ul>
+                    <li>
+                        <a href="/products/1">상품상세 1</a>
+                    </li>
+                    <li>
+                        <a href="/products/2">상품상세 2</a>
+                    </li>
+                    <li>
+                        <a href="/products/3">상품상세 3</a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
 }
