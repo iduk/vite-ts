d2fb6a2873b55d82cd29c730b98e120ab8da7fb5
iduk <iduk.design@gmail.com>
2025-01-13 12:05:36 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import { useRoutes } from 'react-router-dom'
-import { routes } from './routes'
 import 'assets/styles/global.scss'
+import IndexPage from 'pages/index'
+import GlobalLayout from 'pages/_layout'
+import Home from 'pages/home'
+import ProductIndex from 'pages/products'
+import ProductDetail from 'pages/products/[id]'
+import TestIndex from 'pages/test'
 
 const App = () => {
+    const routes = [
+        {
+            path: '/',
+            element: <GlobalLayout />,
+            children: [
+                { path: '/', element: <IndexPage /> },
+                { path: '/home', element: <Home /> },
+                { path: '/products', element: <ProductIndex /> },
+                { path: '/products/:id', element: <ProductDetail /> },
+                { path: '/test', element: <TestIndex /> },
+            ],
+        },
+    ]
     const elem = useRoutes(routes)
     return elem
 }
