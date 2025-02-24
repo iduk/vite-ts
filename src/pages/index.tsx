28c7f126134af3934b462b7b852918d376b4fee4
iduk <iduk.design@gmail.com>
2025-02-24 11:08:30 +0900

    index error 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
-import ImageGrid from 'components/ImageGrid'
+// import ImageGrid from 'components/ImageGrid'
+import Draggable from 'components/Draggable'
 
 export default function Index() {
     return (
         <div className="relative w-full h-screen">
-            <ImageGrid />
+            {/* <ImageGrid /> */}
+
+            <Draggable />
         </div>
     )
 }
